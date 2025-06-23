14f51efc2d1fb5ef2ba808f28a0633f489a9ce23
N.Komkov <komkov_nikita@internet.ru>
2025-06-23 10:33:22 +0300

    Domain , Services : Adding interface for CRUD methods for courses

---
+++ LMS.System.Domain/Services/CRUD for Courses/Interfaces/ICourseRepositiry.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LMS.System.Domain.Services.DBServices.Models;
+
+namespace LMS.System.Domain.Services.CRUD_for_Courses.Interfaces
+{
+    /// <summary>
+    /// Интерфейс репозитория.
+    /// </summary>
+    public interface ICourseRepository
+    {
+        /// <summary>
+        /// Получение курса по ID.
+        /// </summary>
+        /// <param name="id">Айдишник курса.</param>
+        /// <param name="cancellationToken">Токен отмены.</param>
+        /// <returns>Возвращает курс по айдишнику.</returns>
+        Task<Course?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Добавление курса.
+        /// </summary>
+        /// <param name="course">Передаём курс.</param>
+        /// <param name="cancellationToken">Токен отмены.</param>
+        /// <returns>Возвращаем сообщение.</returns>
+        Task<Guid> AddAsync(Course course, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Обновление курса.
+        /// </summary>
+        /// <param name="course">Передаём курс.</param>
+        /// <param name="cancellationToken">Токен отмены.</param>
+        /// <returns>Возвращаем сообщение</returns>
+        Task UpdateAsync(Course course, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Удаление курса.
+        /// </summary>
+        /// <param name="id">Айдишник курса.</param>
+        /// <param name="cancellationToken">Токен отмены.</param>
+        /// <returns>Возвращаем сообщение</returns>
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Публикация курса.
+        /// </summary>
+        /// <param name="id">Айдишник курса.</param>
+        /// <param name="cancellationToken">Токен отмены.</param>
+        /// <returns>Возвращаем сообщение</returns>
+        Task PublishAsync(Guid id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Архивирование курса.
+        /// </summary>
+        /// <param name="id">Айдишник курса.</param>
+        /// <param name="cancellationToken">Токен отмены.</param>
+        /// <returns>Возвращаем сообщение</returns>
+        Task ArchiveAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
