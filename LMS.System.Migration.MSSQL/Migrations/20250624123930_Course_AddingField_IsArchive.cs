a597bffbeeaaf12ea8ce72c91ee466f5053521f4
N.Komkov <komkov_nikita@internet.ru>
2025-06-25 09:49:35 +0300

    Migration Migrations:Adding migration for new field Course

---
+++ LMS.System.Migration.MSSQL/Migrations/20250624123930_Course_AddingField_IsArchive.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.System.Migrations.MSSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Course_AddingField_IsArchive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsArchive",
+                table: "Courses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsArchive",
+                table: "Courses");
+        }
+    }
+}
